65632cc216fb33a7726e1ce98b0750ea9382b9ac
Polyware Sdn Bhd <43948491+Suvindran94@users.noreply.github.com>
2025-12-18 16:08:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -221,4 +221,6 @@ def handle_audio():
 # Run Flask App
 # -------------------------
 if __name__ == "__main__":
-    app.run(debug=True, host="127.0.0.1", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
+
